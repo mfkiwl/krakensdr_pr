f59ea3e3ed8bb97fe1ae347e1d7b2119aa4f4f2d
krakenrf <>
2022-10-27 11:28:39 +0100

    add middleware folder

---
+++ middleware/index.js
@@ -0,0 +1,142 @@
+// This is a Websocket server that is a middleware to interface wth the new Krakensdr app and future things
+
+const express = require('express')
+const ws = require('ws');
+const fs = require('fs');
+
+const app = express()
+const port = 8042
+const wsport = 8021
+const doaInterval = 250    // Interval the clients should get new doa data in ms
+
+const remoteServer = 'kraken.tynet.eu:8044'
+//const remoteServer = 'map.krakenrf.com:2096'
+const settingsJsonPath = 'settings.json'
+
+let lastDoaUpdate = Date.now()
+let settingsJson = {};
+let inRemoteMode = false;
+let wsClient;
+let wsServer;
+
+//load doa settings file
+function loadSettingsJson (){
+  let rawdata = fs.readFileSync(settingsJsonPath);
+  settingsJson = JSON.parse(rawdata);
+  console.log("Loaded Settings from json")
+  console.log("Freq: "+settingsJson.center_freq);
+  console.log("Mode (Data Format): "+settingsJson.doa_data_format);
+  console.log("Name: "+settingsJson.station_id);
+  console.log("KrakenPro Key: "+settingsJson.krakenpro_key);
+  console.log("Lat: "+settingsJson.latitude);
+  console.log("Lon: "+settingsJson.longitude);
+}
+loadSettingsJson();
+
+// if in remote mode connect to server view websocket
+if(settingsJson.doa_data_format == 'Kraken Pro Remote' && settingsJson.krakenpro_key != '0') {
+  console.log("Remote mode activated");
+  inRemoteMode = true;
+}
+
+if(inRemoteMode){
+  wsClient = new ws("wss://"+remoteServer);
+
+  wsClient.onopen = () => {
+    wsClient.send(`{"apikey": "${settingsJson.krakenpro_key}"}`) 
+  }
+   
+  wsClient.onerror = (error) => {
+    console.log('WebSocket error:', error)
+  }
+   
+  wsClient.onmessage = (e) => {
+    //check what data we got from Server
+    var jsn = JSON.parse(e.data);
+    if(jsn.function == 'settings'){
+      console.log("Got new Settings: "+jsn);
+      // read settings fresh from file and set new Settings
+      loadSettingsJson();
+      settingsJson.center_freq = Number.parseFloat(jsn.freq);      
+      fs.writeFileSync(settingsJsonPath, JSON.stringify(settingsJson, null, 2));
+    } else {
+      console.log(jsn);
+    }
+  }
+} else {
+  // when not in Remote mode start websocket server for local connections
+  // Websocket that sends incomming Data to App
+  wsServer = new ws.Server({ noServer: true });
+  wsServer.on('connection', socket => {
+    console.log('Got websocket connection!')
+    socket.on('message', message => {
+        console.log("received: %s",message)
+        //socket.send('Connection works to KrakenSDR')
+      });
+  });
+
+  const server = app.listen(wsport);
+  server.on('upgrade', (request, socket, head) => {
+    wsServer.handleUpgrade(request, socket, head, socket => {
+      wsServer.emit('connection', socket, request);
+    });
+  });
+}
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Hi, this is the KrakenSDR middleware server :)')
+})
+
+app.post('/doapost', (req, res) => {
+  if(Date.now() - lastDoaUpdate > doaInterval){
+    //console.log(req.body);
+    lastDoaUpdate = Date.now()
+    // in remote mode, send data to sdr server backend like the App does
+    if (inRemoteMode) {
+      // In remote mode set lat/lon
+      //req.body.latitude = settingsJson.latitude; 
+      //req.body.longitude = settingsJson.longitude;
+      //req.body.gpsBearing = settingsJson.heading;
+      console.log(req.body.latitude);
+      console.log(req.body.longitude);
+      wsClient.send(`{"apikey": "${settingsJson.krakenpro_key}", "data": ${JSON.stringify(req.body)}}`) 
+    } else {
+      // sends data to all websocket clients
+      wsServer.clients.forEach(function each(client) {
+        if (client.readyState === ws.OPEN) {
+          client.send(JSON.stringify(req.body));
+        }
+      })
+    } 
+
+  } else {
+    console.log("...");
+  }
+  res.sendStatus(200)
+});
+
+app.post('/prpost', (req, res) => {
+    // in remote mode, send data to sdr server backend like the App does
+    if (inRemoteMode) {
+      // In remote mode set lat/lon
+      //req.body.latitude = settingsJson.latitude; 
+      //req.body.longitude = settingsJson.longitude;
+      //req.body.gpsBearing = settingsJson.heading;
+      console.log(req.body);
+      wsClient.send(`{"apikey": "${settingsJson.krakenpro_key}", "type": "pr", "data": ${JSON.stringify(req.body)}}`) 
+    } else {
+      // sends data to all websocket clients
+      wsServer.clients.forEach(function each(client) {
+        if (client.readyState === ws.OPEN) {
+          client.send(JSON.stringify(req.body));
+        }
+      })
+    } 
+  res.sendStatus(200)
+});
+
+app.listen(port, () => {
+    console.log(`Middleware HTTP Server is listening at http://localhost:${port}, Websocket on ${wsport}`)
+})
---
+++ middleware/simulation_from_log.js
@@ -0,0 +1,63 @@
+const axios = require('axios');
+
+const fs = require('fs');
+const { syncBuiltinESMExports } = require('module');
+
+var logArray = [];
+var logIndex = 0;
+try {
+  const data = fs.readFileSync('redoubt360.csv', 'UTF-8');
+  const lines = data.split(/\r?\n/);
+
+  lines.forEach(line => {
+    var logData = line.split(',');
+    
+    var logEntry = {
+      tStamp: parseInt(logData[0]),
+      latitude: parseFloat(logData[1]),
+      longitude: parseFloat(logData[2]),
+      gpsBearing: parseFloat(logData[3]),
+      radioBearing: parseFloat(logData[5]),
+      conf: parseInt(logData[6]),
+      power: parseFloat(logData[7]),
+      freq: parseInt(logData[8]),
+      antType: logData[9],
+      latency: parseInt(logData[10]),
+      doaArray: logData.slice(12, logData.length-1).map(x => parseFloat(x))+",", 
+    };
+/*
+    // new Format
+    var logEntry = {
+      tStamp: parseInt(logData[0]),
+      latitude: parseFloat(logData[8]),
+      longitude: parseFloat(logData[9]),
+      gpsBearing: parseFloat(logData[10]),
+      radioBearing: parseFloat(logData[1]),
+      conf: parseFloat(logData[2]),
+      power: parseFloat(logData[3]),
+      freq: parseInt(logData[4]),
+      antType: logData[5],
+      latency: parseInt(logData[6]),
+      doaArray: logData.slice(17, logData.length).map(x => parseFloat(x))+",",  
+    };*/
+    //console.log(logEntry.doaArray);
+    logArray.push(logEntry);
+  });
+
+} catch (err) {
+  console.error(err);
+}
+
+setInterval(() => {
+if (logIndex <= logArray.length) {
+    console.log("Sending Data as kraken: "+logArray[logIndex])
+    axios.post('http://localhost:8042/doapost', logArray[logIndex])
+        .then(function (response) {
+            //console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    logIndex++;
+}
+}, 1000);
